d823ea8308bcbe69a52aae5f3a78184d0f8d2c64
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2025-05-13 16:22:49 +0800

    memperbaiki tampilan admin

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -1,7 +1,3 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.2.0
-    </div>
+<footer class="text-center main-footer justify-content-center">
+    <a class="text-center" href="https://instagram.com/bolonsite">bolonsite</a>.
 </footer>
--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -1,5 +1,11 @@
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+    </ul>
 
     <!-- Right navbar links -->
     <ul class="ml-auto navbar-nav">
@@ -8,7 +14,6 @@
                 <i class="fas fa-expand-arrows-alt"></i>
             </a>
         </li>
-        
     </ul>
 </nav>
 <!-- /.navbar -->
