ea9253f016115b1c8d2deceeb795666314f97c00
gilbertsimbolon <gilbertsimbolon306@gmail.com>
2025-05-14 13:29:56 +0800

    memperbaiki judul sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,7 +1,8 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="/admin" class="brand-link">
-        <span class="brand-text font-weight-light">Website Monitoring | BPN</span>
+        <img src="{{ asset('img/logo-bpn.png') }}" alt="Logo BPN" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Website Monitoring</span>
     </a>
     <div class="sidebar">
         <nav class="mt-2">
@@ -15,7 +16,7 @@
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
-                        <li class="nav-item" style="padding-left: 30px">
+                        <li class="nav-item" style="padding-left: 35px">
                             <a href="/data-pengguna" class="nav-link">
                                 <p>Pengguna</p>
                             </a>
